1bafb8216c114957db10c3a30e2740f1613d8f28
ผศ.ดร. เอกรัฐ รัฐกาญจน์ <69342162+ekaratnida@users.noreply.github.com>
2026-04-23 10:09:30 +0700

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC123.')
+st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -10,8 +10,7 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 
 @st.cache_data
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL,nrows=nrows)
-    st.write(data.shape)
+    data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
@@ -22,7 +21,7 @@ data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
-data_load_state.text('Done! (using st.cache_data)')
+data_load_state.text('Loading data...done!')
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
@@ -30,16 +29,28 @@ if st.checkbox('Show raw data'):
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour,
-    bins=24,
-    range=(0,24)
-)[0]
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 #st.write(hist_values)
-st.bar_chart(hist_values)
+st.bar_chart(hist_values) #How to use plotly bar here?
+
+'''
+import plotly.express as px
+
+df = px.data.iris()
+fig = px.histogram(
+    x=np.histogram(
+        data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[1][0:24],
+        y=hist_values,
+        nbins=24
+    )
+
+st.plotly_chart(fig)
+'''
 
 #st.subheader('Map of all pickups')
 #st.map(data)
-hour_to_filter = st.slider('hour',0,23,17)
-filtered_data = data[data[DATE_COLUMN].dt.hour==hour_to_filter]
+#hour_to_filter = 17
+hour_to_filter = st.sidebar.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
+st.map(filtered_data)   
