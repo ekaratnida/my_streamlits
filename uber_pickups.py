85e7c2ba536e0b4ac109c16eb465e2874ade1bfd
ผศ.ดร. เอกรัฐ รัฐกาญจน์ <69342162+ekaratnida@users.noreply.github.com>
2026-04-26 14:00:34 +0700

    Refactor Uber pickups Streamlit app

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import numpy as np 
 
-st.title('Uber pickups in NYC (DADS9-2568)')
+st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -16,41 +16,34 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-# Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+data_load_state.text('Loading data ... done!')
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
+    #st.table(data)
+    #st.dataframe(data)
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-#st.write(hist_values)
-st.bar_chart(hist_values) #How to use plotly bar here?
+st.bar_chart(hist_values)
 
-'''
-import plotly.express as px
 
-df = px.data.iris()
-fig = px.histogram(
-    x=np.histogram(
-        data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[1][0:24],
-        y=hist_values,
-        nbins=24
-    )
+#import plotly.express as px
+#fig = px.histogram(
+#        x=np.histogram(data[DATE_COLUMN].dt.hour, 
+#                   bins=24, range=(0,24))[1][0:24], 
+#        y=hist_values, nbins=24 )
+#st.plotly_chart(fig)
 
-st.plotly_chart(fig)
-'''
 
 #st.subheader('Map of all pickups')
 #st.map(data)
-#hour_to_filter = 17
-hour_to_filter = st.sidebar.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+
+hour_to_filter = st.slider('hour',0,23,17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)   
+st.map(filtered_data)
